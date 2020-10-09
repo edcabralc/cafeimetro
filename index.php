30834b68169eac095ff9e9b25ae81eaccc340e41
edcabralc <cabralnet@gmail.com>
2020-10-09 14:12:31 -0300

    Adicionando arquivos

---
+++ index.php
@@ -0,0 +1,15 @@
+
+
+
+
+<?php
+
+echo ("Ola mundo agora essa porra tá funcionanado");
+// mysqli_connect();
+$numero = 0;
+?>
+
+
+<?php if ($numero < 2): ?>
+<?php echo ($numero); ?>
+<?php endif; ?>
