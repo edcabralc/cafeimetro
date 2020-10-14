28ae38d23b3159d89beac5863661008da6496c69
edcabralc <cabralnet@gmail.com>
2020-10-14 00:57:43 -0300

    sync

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-<!-- Image and text -->
+<!-- Image and text
 <nav class="navbar navbar-light bg-light">
   <a class="navbar-brand" href="#">
     <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
     Bootstrap
   </a>
-</nav>    
+</nav>     -->
     
     
     <!-- Optional JavaScript -->
